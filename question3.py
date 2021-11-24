1629ef0772e9f4e26c03129b36dd1b956dc782fc
oceallaighsea <83773002+oceallaighsea@users.noreply.github.com>
2021-11-24 14:39:32 +0000

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,5 @@
+#Question3.py
+#1)Using a for loop- Print out the numbers 1-10 on a new line
+
+#2) print 1-10 on the same line separated by a * 3) print 1-10 on same line separated by a + at the end give the answer
+#output 1+2+3+4+5+6+7+8+9+10 = answer..
