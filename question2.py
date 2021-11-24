2a95fe5aeffa63090f56a7fad55840dc8bde091a
oceallaighsea <83773002+oceallaighsea@users.noreply.github.com>
2021-11-24 14:36:59 +0000

    Create question2.py

---
+++ question2.py
@@ -0,0 +1,8 @@
+# Question 2
+# Use a for loop and the range function to print out items a specific number of times.
+
+# a)Using a for loop print out your name 10 times
+
+# b) print out the number 33 4 times
+
+# c) print out "we are the champions" 100 times
