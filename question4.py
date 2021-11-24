34b360cfb3442a5d6648dd2dc40de8203a06a822
oceallaighsea <83773002+oceallaighsea@users.noreply.github.com>
2021-11-24 14:40:57 +0000

    Create question4.py

---
+++ question4.py
@@ -0,0 +1,10 @@
+#Question 4.py
+#1-Using a for loop print out each of the letters in the string "Today is Friday" on a new line.(p36)
+
+#2- Print each of the letters on the same line with a space between them
+#output = "T o d a y i s F r i d a y"
+
+# 3 - print out the same string with a - between each letter
+
+#4 - Print out the same string - make every second letter capital
+# outout = "ToDaY Is fRiDaY". (pay attention to F..)
