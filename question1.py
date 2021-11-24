bfba388f4b7092921f60ee9b040d6044e7674934
oceallaighsea <83773002+oceallaighsea@users.noreply.github.com>
2021-11-24 14:32:06 +0000

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,15 @@
+# For loops are given a specific number of times to repeat:
+# Using the range(start, end) function complete the following tasks
+# Question1.py
+
+# a) list out all the numbers from 1 - 10
+
+# b) list all the odd numbers between 1 - 10
+
+# c) list all the even numbers between 1 - 10
+
+# d) add all the numbers between 1 - 10
+
+# e) add all the even numbers between 1 - 10
+
+# f) add all the uneven numbers between 1 - 10
